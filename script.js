d709f5f5e3aeb228cc2e8a239ed81c36172a5720
Alan <142617650+alzhang16@users.noreply.github.com>
2025-01-08 21:01:06 -0800

    Add player input and returns victory or loss
    Prompts player to type in response and will keep
    track of player score + computer score

--- script.js
+++ script.js
@@ -3,16 +3,55 @@ function getComputerChoice(){
     let choice = "";
     if(randChoice <= 1){
         choice = "Rock";
-    }else if(randChoice <= 2){
+    }else if(randChoice <= 2){ 
         choice = "Paper";
     }else{
         choice = "Scissors";
     }
-    return randChoice;
+    return choice.toLowerCase();
 }
 
 function getHumanChoice(){
-    
+    let humanChoice = prompt("Rock, Paper, or Scissors?").toLowerCase(); 
+
+    if(humanChoice == "rock" || humanChoice == "paper" || humanChoice == "scissors"){
+        return humanChoice;
+    }else{
+        console.log("Please enter a valid value");
+        getHumanChoice();
+    }
+
 }
 
-console.log(getComputerChoice());
+function playRound(humanChoice, computerChoice){
+    console.log(humanChoice);
+    console.log(computerChoice);
+    if(humanChoice == computerChoice){
+        console.log("Draw");
+    }else if(humanChoice == "rock" && computerChoice == "scissors"){
+        console.log("Win!");
+        humanScore+=1;
+    }else if(humanChoice == "rock" && computerChoice == "paper"){
+        computerScore+=1;
+        console.log("Lost!");
+    }else if(humanChoice == "paper" && computerChoice == "rock"){
+        console.log("Win!");
+        humanScore+=1;
+    }else if(humanChoice == "paper" && computerChoice == "scissors"){
+        computerScore+=1;
+        console.log("Lost!");
+    }else if(humanChoice == "scissors" && computerChoice == "paper"){
+        console.log("Win!");
+        humanScore+=1;
+    }else if(humanChoice == "scissors" && computerChoice == "rock"){
+        computerScore+=1;
+        console.log("Lost!");
+    }
+}  
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+let humanScore = 0;
+let computerScore = 0;
+
+playRound(humanSelection, computerSelection);
